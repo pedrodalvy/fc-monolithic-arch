268be7fbd9926aa85ac335e965bad24bbc50108d
Pedro Dalvy <pedrodalvy@outlook.com>
2023-12-09 23:31:53 -0300

    chore: config jest

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+/** @type {import('jest').Config} */
+const config = {
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!**/*.interface.ts', '!**/*.dto.ts'],
+  coverageReporters: ['lcov', 'text', 'clover'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+};
+
+module.exports = config;
