07c00b7e3b8d132f951a1a9a286a14f5eb4e8221
Pedro Dalvy <pedrodalvy@outlook.com>
2023-12-28 22:23:44 -0300

    chore(wip): get store catalog products in place order use case

--- src/modules/checkout/usecase/place-order/place-order.usecase.spec.ts
+++ src/modules/checkout/usecase/place-order/place-order.usecase.spec.ts
@@ -1,7 +1,10 @@
 import { mock, MockProxy } from 'jest-mock-extended';
 
+import ID from '../../../@shared/domain/value-object/id.value-object';
 import ClientAdmFacadeInterface from '../../../client-adm/facade/client-adm-facade.interface';
 import ProductAdmFacadeInterface from '../../../product-adm/facade/product-adm-facade.interface';
+import StoreCatalogFacadeInterface from '../../../store-catalog/facade/store-catalog.facade.interface';
+import Product from '../../domain/product.entity';
 import { PlaceOrderInputDTO } from './place-order.dto';
 import PlaceOrderUseCase from './place-order.usecase';
 
@@ -10,14 +13,16 @@ describe('PlaceOrderUseCase unit test', () => {
 
   let clientFacade: MockProxy<ClientAdmFacadeInterface>;
   let productFacade: MockProxy<ProductAdmFacadeInterface>;
+  let storeCatalogFacade: MockProxy<StoreCatalogFacadeInterface>;
 
   let placeOrderInput: PlaceOrderInputDTO;
 
   beforeEach(() => {
     clientFacade = mock<ClientAdmFacadeInterface>();
     productFacade = mock<ProductAdmFacadeInterface>();
+    storeCatalogFacade = mock<StoreCatalogFacadeInterface>();
 
-    useCase = new PlaceOrderUseCase(clientFacade, productFacade);
+    useCase = new PlaceOrderUseCase(clientFacade, productFacade, storeCatalogFacade);
 
     placeOrderInput = { clientId: 'client-id', products: [{ productId: 'product-id' }] };
   });
@@ -45,6 +50,18 @@ describe('PlaceOrderUseCase unit test', () => {
       await expect(useCase.execute(placeOrderInput)).rejects.toThrow(new Error('An error'));
       expect(validateProductsSpy).toHaveBeenCalled();
     });
+
+    it('should throw an error when product is not found', async () => {
+      // Arrange
+      clientFacade.findClient.mockResolvedValueOnce(true as any);
+      jest.spyOn<any, any>(useCase, '_validateProducts').mockImplementationOnce(jest.fn());
+
+      const getProductSpy = jest.spyOn<any, any>(useCase, '_getProduct').mockRejectedValueOnce(new Error('An error'));
+
+      // Assert
+      await expect(useCase.execute(placeOrderInput)).rejects.toThrow(new Error('An error'));
+      expect(getProductSpy).toHaveBeenCalled();
+    });
   });
 
   describe('_validateProducts method', () => {
@@ -71,6 +88,39 @@ describe('PlaceOrderUseCase unit test', () => {
       expect(productFacade.checkStock).toHaveBeenCalledTimes(2);
       expect(productFacade.checkStock).toHaveBeenNthCalledWith(1, { productId: 'product-id-1' });
       expect(productFacade.checkStock).toHaveBeenNthCalledWith(2, { productId: 'product-id-2' });
+    });
+  });
+
+  describe('_getProduct method', () => {
+    it('should throw an error when product is not found', async () => {
+      // Arrange
+      storeCatalogFacade.find.mockResolvedValueOnce(undefined);
+
+      // Assert
+      await expect(useCase['_getProduct']('invalid-id')).rejects.toThrow('Product not found');
+    });
+
+    it('should return a product', async () => {
+      // Arrange
+      storeCatalogFacade.find.mockResolvedValueOnce({
+        id: 'product-id',
+        name: 'Product 1',
+        description: 'Product 1',
+        salesPrice: 10,
+      });
+
+      // Assert
+      const product = await useCase['_getProduct']('product-id');
+
+      expect(storeCatalogFacade.find).toHaveBeenCalledTimes(1);
+      expect(product).toEqual(
+        new Product({
+          id: new ID('product-id'),
+          name: 'Product 1',
+          description: 'Product 1',
+          salesPrice: 10,
+        }),
+      );
     });
   });
 });
--- src/modules/checkout/usecase/place-order/place-order.usecase.ts
+++ src/modules/checkout/usecase/place-order/place-order.usecase.ts
@@ -1,12 +1,16 @@
+import ID from '../../../@shared/domain/value-object/id.value-object';
 import UseCaseInterface from '../../../@shared/usecase/use-case.interface';
 import ClientAdmFacadeInterface from '../../../client-adm/facade/client-adm-facade.interface';
 import ProductAdmFacadeInterface from '../../../product-adm/facade/product-adm-facade.interface';
+import StoreCatalogFacadeInterface from '../../../store-catalog/facade/store-catalog.facade.interface';
+import Product from '../../domain/product.entity';
 import { PlaceOrderInputDTO as InputDTO, PlaceOrderOutputDTO as OutputDTO } from './place-order.dto';
 
 export default class PlaceOrderUseCase implements UseCaseInterface<InputDTO, OutputDTO> {
   constructor(
     private readonly clientFacade: ClientAdmFacadeInterface,
     private readonly productFacade: ProductAdmFacadeInterface,
+    private readonly storeCatalogFacade: StoreCatalogFacadeInterface,
   ) {}
 
   async execute(input: InputDTO): Promise<OutputDTO> {
@@ -14,6 +18,8 @@ export default class PlaceOrderUseCase implements UseCaseInterface<InputDTO, Out
     if (!client) throw new Error('Client not found');
 
     await this._validateProducts(input);
+
+    const orderProducts = await Promise.all(input.products.map(product => this._getProduct(product.productId)));
 
     return undefined;
   }
@@ -29,5 +35,17 @@ export default class PlaceOrderUseCase implements UseCaseInterface<InputDTO, Out
         throw new Error(`Product ${product.productId} is not available in stock`);
       }
     }
+  }
+
+  private async _getProduct(productId: string): Promise<Product> {
+    const product = await this.storeCatalogFacade.find({ id: productId });
+    if (!product) throw new Error('Product not found');
+
+    return new Product({
+      id: new ID(product.id),
+      name: product.name,
+      description: product.description,
+      salesPrice: product.salesPrice,
+    });
   }
 }
