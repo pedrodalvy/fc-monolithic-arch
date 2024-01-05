1769cb52cc0c24bc85930b6af78eff8ef43a9d11
Pedro Dalvy <pedrodalvy@outlook.com>
2024-01-05 11:46:35 -0300

    feat: add endpoint to create products

--- src/infrastructure/api/app.ts
+++ src/infrastructure/api/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 
 import clientRoutes from './routes/client.routes';
+import productRouter from './routes/product.routes';
 
 export const app = express();
 
 app.use(express.json());
 app.use('/clients', clientRoutes);
+app.use('/products', productRouter);
