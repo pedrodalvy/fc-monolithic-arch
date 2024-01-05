71c007dbcac6b192526118efd570040f57dc65e6
Pedro Dalvy <pedrodalvy@outlook.com>
2024-01-05 10:45:58 -0300

    feat: add endpoint to create clients

--- src/infrastructure/api/server.ts
+++ src/infrastructure/api/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 
 import DatabaseConnection from '../database/sequelize/database.connection';
+import clientRoutes from './routes/client.routes';
 
 const app = express();
 app.use(express.json());
+
+app.use('/clients', clientRoutes);
 
 DatabaseConnection.connect().then(() => {
   console.log('Database connected');
